5b5105ef28476b5635c70f3038bab016748728c3
karthi25kuk <karthikeyankannan.vnr@gmail.com>
2025-03-22 10:41:31 +0530

    updated the no of attempts

--- number gussing game.py
+++ number gussing game.py
@@ -1,6 +1,8 @@
 import random
 ans=random.randint(1,100)
+attempt=0
 while True:
+    attempt+=1
     num=int(input("enter a number fron 1 to 100:"))
     if(num>=1 and num<=100):
         if num>ans:
@@ -8,7 +10,7 @@ while True:
         elif num<ans:
             print("it is low!")
         else:
-            print("yes you guessed!")
+            print("yes you guessed! in ",attempt," attempts!")
             break
     else:
         print("invalid!")

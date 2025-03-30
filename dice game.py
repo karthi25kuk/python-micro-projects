6a0c4e4af24269a282948e9794c820f68ca52d03
karthi25kuk <karthikeyankannan.vnr@gmail.com>
2025-03-30 20:41:09 +0530

    dice game added

---
+++ dice game.py
@@ -0,0 +1,13 @@
+import random as rm
+
+print("enter \"y\" to role dice,\"n\" to stop game.")
+while True:
+    inp=input("enter a choice:").strip().lower()
+    if inp=='y':
+        value=rm.randint(1,6)
+        print(value)
+    elif inp=='n':
+        print("thanks for rolling!")
+        break
+    else:
+        print("invalid choice!")
